79a356019c714b672bc2a54d612c630c9750cf7b
Ayesh Gunawardana <42178947+ayeshnayana@users.noreply.github.com>
2020-12-01 14:15:13 -0500

    First Commit
    Developed the player scipt and environment

---
+++ quantum/Assets/script/create_electron.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class create_electron : MonoBehaviour
+{
+    public Transform ePos;
+    public GameObject electronee;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H)) 
+        {
+            Instantiate(electronee, ePos.position, ePos.rotation);
+        }
+    }
+}
---
+++ quantum/Assets/script/electron.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class electron : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Key H press for hadamart operator
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            GetComponent<Rigidbody>().position = new Vector3(4.66f, 1.36f, 0.038f);
+        }
+    }
+}
